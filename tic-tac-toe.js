d1da173f227d02b02fb5751e9ec4f65ce1109418
Rushawn Campbell <rushawn.campbell@mymona.uwi.edu>
2021-10-09 19:42:03 -0500

    Completed Winner checking, Anti-cheat and game reset feature

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,15 @@
 window.addEventListener("load", (event)=> {
+    
     let winnerCheck = [[0,1,2],[3,4,5], [6,7,8], 
-    [0,3,6], [1,4,7], [2,5,8], [0,4,8] , [2,5,6]];
+    [0,3,6], [1,4,7], [2,5,8], [0,4,8] , [2,4,6]];
+
     let xWinnerVal = 0;
     let oWinnerVal = 0;
+    let playerX= true;
+    let playerO = false;
     let active;
     let squares  = document.querySelectorAll("#board > div");
     let bton =  document.querySelector(".btn");
-    let state = -1;
 
     squares.forEach((sq)=> {
         sq.classList.add("square");
@@ -14,52 +17,39 @@ window.addEventListener("load", (event)=> {
     
     });
 
-    
-
     squares.forEach((sq)=> {
+        
         sq.onclick = (event)=>{
-            selected = 0;
-            if (state == -1 || state == 1){
-                sq.classList.remove("O");
+            if (playerX && sq.innerHTML == " "){
                 sq.classList.add("X");
                 sq.innerHTML = 'X';
-                state = 0;
+                playerX = false;
+                playerO = true;
             }
-            else {
-                sq.classList.remove("X");
+            else if (playerO && sq.innerHTML == " ") {
                 sq.classList.add("O");
                 sq.innerHTML = 'O';
-                state = 1;
+                playerO = false;
+                playerX = true;
             }
-
-
-            while(selected <= squares.length){
-                if (squares[selected] == sq){
-                    break;
-                }
-                selected++;
-            } 
-
+            
             active = checkWinner();
 
             if (active == 1){
-                xWinnerVal++;
-            }
-            else if (active == 0){
-                oWinnerVal++;
-            }
-            
-            if (xWinnerVal == 3){
-                console.log("X won");
-            }
-            else if (oWinnerVal == 3){
-                console.log("O won");
-            }
+               document.querySelector("div#status").classList.add("you-won");
+               document.querySelector("div#status").innerHTML = `Congratulations! ${sq.innerHTML} is the Winner!`;
 
+               squares.forEach((square)=>{
+                   square.onclick = (event) => {
+                       event.preventDefault();
+                   }
+               })
+
+            }
         }
 
         sq.onmouseover = (event)=>{
-            sq.style.transition = "all .3s ease-in-out"
+            sq.style.transition = "all .3s ease-in-out";
             sq.classList.add("hover");
         }
 
@@ -67,42 +57,39 @@ window.addEventListener("load", (event)=> {
             sq.classList.remove("hover");
         }
 
+        let checkWinner =()=> {
 
-        let checkWinner =()=>{
-            let status = -1;
-            for (item = 0; item < winnerCheck.length; item++){
-                
-                winnerCheck[item].forEach((itemlet) => {
-                    if (selected == itemlet){
-                        status = 1;
+            for (item = 0 ; winnerCheck.length; item++){
+
+                winnerCheck[item].forEach(element => {
+                    if (squares[element].classList.contains("X")){
+                        xWinnerVal++;
                     }
-                } );
-
-                if (status == 1){
-                    console.log("yes");
-                    for (let itemlet = 0; itemlet < winnerCheck[item].length; itemlet++){
-                        if (squares[winnerCheck[item][itemlet]].innerHTML == "X"){
-                            return 1;
-                            //break;
-                        }
-                        else if (squares[winnerCheck[item][itemlet]].innerHTML == "O"){
-                            return 0;
-                            //break;
-                        }
-
+                    else if (squares[element].classList.contains("O")){
+                        oWinnerVal++;
                     }
-                        
+                });
+                if(xWinnerVal == 3 || oWinnerVal ==3 ){
+                    return 1;
                 }
-                else{
-                    continue;
-                } 
+                xWinnerVal = 0;
+                oWinnerVal = 0;
+               if (item < winnerCheck.length - 1){
+                   continue;
+               }
+               return 0;
+
             }
-        }
+         } 
         
     });
 
     bton.addEventListener('click', (event) => {
-        squares.forEach((sq) => sq.innerHTML = "");
+        
+        squares.forEach((sq) => {
+            sq.innerHTML = "";
+            location.reload();
+        });
+
     })
 });
-
